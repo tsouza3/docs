60c30c80760b6901810688321e140602ddceeff5
Souza <thiagodsouza.3@gmail.com>
2024-01-20 07:25:08 -0300

    v1

---
+++ servidor/db/dbConnect.js
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+
+const cliente = new MongoClient(
+  "mongodb+srv://Souza:souza123@docs.ewsymlv.mongodb.net/?retryWrites=true&w=majority"
+);
+
+let documentosColecao, usuariosColecao;
+
+try {
+  await cliente.connect();
+
+  const db = cliente.db("docs");
+  documentosColecao = db.collection("documentos");
+  usuariosColecao = db.collection("usuarios");
+
+  console.log("Conectado ao banco de dados com sucesso!");
+} catch (erro) {
+  console.log(erro);
+}
+
+export { documentosColecao, usuariosColecao };
